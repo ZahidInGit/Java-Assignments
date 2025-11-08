a26c6efc0cdad1279725dfa8f8b5de9b938a4932
ZahidInGit <it24012@mbstu.ac.bd>
2025-11-08 22:24:55 +0600

    Create CustomPrintClass.java

---
+++ Assignment 8/ICT/22/ClassNames/CustomPrintClass.java
@@ -0,0 +1,8 @@
+package ict._22.ClassNames;
+
+public class CustomPrintClass {
+    public static CustomPrintClass a = new CustomPrintClass();
+    public void pr(Object msg) {
+        System.out.println(msg);
+    }
+}
