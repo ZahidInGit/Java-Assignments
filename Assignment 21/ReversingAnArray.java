8ac67c59fc0ced6d1ff30fdf5839bd9a74a56ea3
ZahidInGit <it24012@mbstu.ac.bd>
2026-01-27 23:06:33 +0600

    Update ReversingAnArray.java

--- Assignment 21/ReversingAnArray.java
+++ Assignment 21/ReversingAnArray.java
@@ -3,7 +3,6 @@ public class ArrayUtils {
         int start = 0;
         int end = arr.length - 1;
 
-        // Swap elements from the outside in
         while (start < end) {
             double temp = arr[start];
             arr[start] = arr[end];
@@ -18,7 +17,6 @@ public class ArrayUtils {
         double[] data = {5.8, 2.6, 9.0, 3.4, 7.1};
         reverse(data);
         
-        // Print the result to verify
         for (double val : data) {
             System.out.print(val + " ");
         }
