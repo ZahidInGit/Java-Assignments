f0f5c6e294e582bb9f494b2a45cf5a41e3178c9f
ZahidInGit <it24012@mbstu.ac.bd>
2026-01-28 15:17:29 +0600

    Create Rectangle.java

---
+++ Lab Final/Interface/Rectangle.java
@@ -0,0 +1,19 @@
+public class Rectangle extends Shape implements Drawable {
+    double length, width;
+
+    public Rectangle(String color, double length, double width) {
+        super(color);
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double area() {
+        return length * width;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a " + color + " Rectangle.");
+    }
+}
