7289137c62d6e6387266ffe066f3a75c66ca8f5e
ZahidInGit <it24012@mbstu.ac.bd>
2026-01-28 15:14:24 +0600

    Update and rename Code to Circle.java

---
+++ Lab Final/Interface/Circle.java
@@ -0,0 +1,18 @@
+public class Circle extends Shape implements Drawable {
+    double radius;
+
+    public Circle(String color, double radius) {
+        super(color);
+        this.radius = radius;
+    }
+
+    @Override
+    public double area() {
+        return Math.PI * radius * radius;
+    }
+
+    @Override
+    public void draw() {
+        System.out.println("Drawing a " + color + " Circle.");
+    }
+}
