7a62ddef6e31a8c4f0ea09a4f101b90c72391518
ZahidInGit <it24012@mbstu.ac.bd>
2026-01-28 15:29:54 +0600

    Update Student.java

--- Lab Final/Encapsulation & Validation/Student.java
+++ Lab Final/Encapsulation & Validation/Student.java
@@ -1,1 +1,27 @@
+public class Student {
+    private int id;
+    private String name;
+    private double cgpa;
 
+    public Student(int id, String name, double cgpa) {
+        this.id = id;
+        this.name = name;
+        setCgpa(cgpa);
+    }
+
+    public int getId() { return id; }
+    public String getName() { return name; }
+    public double getCgpa() { return cgpa; }
+
+    public void setId(int id) { this.id = id; }
+    public void setName(String name) { this.name = name; }
+
+    public void setCgpa(double cgpa) {
+        if (cgpa >= 0.0 && cgpa <= 4.0) {
+            this.cgpa = cgpa;
+        } else {
+            System.out.println("Error: CGPA must be between 0.0 and 4.0. Setting default 0.0.");
+            this.cgpa = 0.0;
+        }
+    }
+}
