47ddbec9fa04c886c2cf1d41a6326132ca954d69
ZahidInGit <it24012@mbstu.ac.bd>
2026-01-28 15:20:21 +0600

    Create Employee.java

---
+++ Lab Final/Inheritance & Method Overriding/Employee.java
@@ -0,0 +1,11 @@
+public class Employee {
+    String name;
+
+    public Employee(String name) {
+        this.name = name;
+    }
+
+    public double calculateSalary() {
+        return 0.0;
+    }
+}
