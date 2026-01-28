478ef72dcef149f70b576a3f82eda7453fdac290
ZahidInGit <it24012@mbstu.ac.bd>
2026-01-28 15:29:28 +0600

    Update Main.java

--- Lab Final/Encapsulation & Validation/Main.java
+++ Lab Final/Encapsulation & Validation/Main.java
@@ -1,1 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Student s1 = new Student(101, "Alice", 3.8);
+        System.out.println("Student: " + s1.getName() + ", CGPA: " + s1.getCgpa());
 
+        Student s2 = new Student(102, "Bob", 5.0);
+        System.out.println("Student: " + s2.getName() + ", CGPA: " + s2.getCgpa());
+    }
+}
